778ca8437c5b6c7aca1ee303694d0a2084a58e1a
Mustapha <mustapha.yinusa@yahoo.com>
2024-08-10 23:09:34 +0100

    Pascal's triangle task

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -14,7 +14,8 @@ def pascal_triangle(n):
             for i in range(1, n):
                 temp_triangle = [1]
                 for j in range(1, i):
-                    temp_triangle.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+                    temp_triangle.append(triangle[i - 1]
+                                         [j - 1] + triangle[i - 1][j])
                 temp_triangle.append(1)
                 triangle.append(temp_triangle)
         return triangle
